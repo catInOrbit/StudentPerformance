ae33412c2fb3aa493d05a57b486702beb8dda235
Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
2021-12-18 02:20:23 +0700

    update

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import pandas as pd
 import tensorflow as tf
 import missingno as msno
 import matplotlib.pyplot as plt
-data = pd.read_csv("student-por.csv", delimiter=";")
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
 
+data = pd.read_csv("student-por.csv", delimiter=";")
 
 print(data.head())
 print(data.info())
@@ -21,7 +23,12 @@ plt.show()
 
 X_data = data_encoded[feature_cols]
 y_data = data_encoded["G3"]
+chi_test = SelectKBest(score_func=chi2, k=6)
 
+fit = chi_test.fit(X_data, y_data)
+cols = fit.get_support(indices=True)
+features_df_new = X_data.iloc[:,cols]
+print(features_df_new)
 
 model_comparison("RE_LE", 10, X_data, y_data)
 model_comparison("RE_Reg", 10, X_data, y_data)
